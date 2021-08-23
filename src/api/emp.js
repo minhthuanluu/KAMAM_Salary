b43d1e951c53d96c4eff51d45ab24567e9a4ce39
Fux Wa <dophuchoa1997vn@gmail.com>
2021-08-23 16:04:51 +0700

    add API ProductivitySub Screen

--- src/api/emp.js
+++ src/api/emp.js
@@ -214,3 +214,38 @@ export const getKamPTRevenue = async (month) => {
         });
     return data;
 };
+
+export const getProductivitySub = async (month) => {
+    let data = baseData
+    await axios({
+        method: "POST",
+        url: `${baseUrl}mobile/follow-impl-plan/avg-productivity/getProductivitySub?month=${month}`,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: getToken(),
+        },
+    })
+        .then(async (res) => {
+            if (res.status == 200) {
+                if (Object.values(res.data).length > 0) {
+                    data = {
+                        data: res.data,
+                        isLoading: false,
+                        status: "success",
+                        error: null
+                    };
+                }
+            }
+        })
+        .catch(async (error) => {
+            console.log(error)
+            data = {
+                message: error.response.data.message,
+                isLoading: false,
+                status: "failed",
+                error: error.response.data
+            };
+        });
+    return data;
+};
--- src/screens/EmpHome/PlanFollow/ProductivitySub/index.js
+++ src/screens/EmpHome/PlanFollow/ProductivitySub/index.js
@@ -1,35 +1,104 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, Text, View, Image} from 'react-native';
-import { Body, DatePicker, GenaralItem, Header } from '../../../../comps';
+import { SafeAreaView, ScrollView, StatusBar, Text, View, Image, FlatList } from 'react-native';
+import { Body, DatePicker, GenaralItem, Header, Loading } from '../../../../comps';
 import { useNavigation } from '@react-navigation/core';
 import { fontScale } from '../../../../utils/Fonts';
 import { width } from '../../../../utils/Dimenssion';
 import { colors } from '../../../../utils/Colors';
 import { styles } from './style';
 import { images } from '../../../../utils/Images';
+import { getMonth } from '../../../../utils/Logistics';
+import { _storeData } from '../../../../utils/Storage';
+import { showToast } from '../../../../utils/toast';
+import Toast from 'react-native-toast-message';
+import { getProductivitySub } from '../../../../api/emp';
+
 const ProductivitySub = (props) => {
     const navigation = useNavigation();
     const [month, setMonth] = useState(moment(new Date()).subtract(1, 'months').format("MM/YYYY"));
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [icon, setIcon] = useState(images.none);
+    const getDatePikerValue = async () => {
+        let monthStore = await getMonth()
+        if (monthStore != undefined) {
+            setMonth(monthStore)
+            getData(monthStore)
+        } else {
+            getData(month)
+        }
+    }
+
+    const getData = async (month) => {
+        setLoading(true)
+        let res = await getProductivitySub(month)
+        if (res.status == "success") {
+            // showToast("success", "Thành công", "Lấy dữ liệu thành công")
+            setData(res.data.data)
+            console.log(res.data.data)
+            setLoading(false)
+        } else {
+            showToast("error", "Lỗi hệ thống", res.message)
+            setLoading(false)
+        }
+    }
+
+    const onChangeDatePicker = (date) => {
+        setData([])
+        setMonth(date)
+        getData(date)
+        _storeData("month", date)
+    }
+    useEffect(() => {
+        navigation.addListener('focus', async () => {
+            await getDatePikerValue()
+        })
+    })
+    const checkIcon = (icon) => {
+        if (icon == "am") {
+            return images.AM_KAM
+        } else if (icon == "shop") {
+            return images.store
+        } else if (icon == "branch") {
+            return images.branch
+        } else if (icon == "company") {
+            return images.company
+        } else {
+            return images.none
+        }
+    }
     return (
         <SafeAreaView style={styles.container}>
+            <Toast style={{ position: "absolute", zIndex: 100 }} ref={(ref) => Toast.setRef(ref)} />
             <StatusBar translucent backgroundColor={colors.primary} />
             <Header title="Năng suất bình quân" />
             <View style={{ alignSelf: "center" }}>
-                <DatePicker month={month} width={width - fontScale(120)} onChangeDate={(date) => setMonth(date)} />
+                <DatePicker month={month} width={width - fontScale(120)} onChangeDate={(date) => onChangeDatePicker(date)} />
             </View>
             <Body style={{ marginTop: fontScale(44) }} showInfo={false} />
             <View style={styles.body}>
-                <ScrollView>
-                    <GenaralItem shopName="Công ty 2" icon={images.company} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" />
+                {/* <ScrollView> */}
+                {/* <GenaralItem shopName="Công ty 2" icon={images.company} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" />
                     <GenaralItem shopName="Công ty 2" icon={images.branch} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" />
                     <GenaralItem shopName="Công ty 2" icon={images.store} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" />
-                    <GenaralItem shopName="Công ty 2" icon={images.AM_KAM} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" />
+                    <GenaralItem shopName="Công ty 2" icon={images.AM_KAM} standardSub="800/26" realSub="600/24" standardRevenue="800/26" realRevenue="600/24" /> */}
+                <FlatList
+                    data={data}
+                    keyExtractor={(item, key) => key.toString()}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <GenaralItem shopName={item.shopName}
+                                icon={checkIcon(item.shopType)}
+                                standardSub={item.standardSub} realSub={item.realSub}
+                                standardRevenue={item.standardRevenue} realRevenue={item.realRevenue} />
+                        )
 
-                </ScrollView>
+                    }}
+                />
             </View>
-
+            <Loading loading={loading} />
         </SafeAreaView>
     );
 }
