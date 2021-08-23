7702bc5d0f31ccb82805ec5712f74f30df23643a
Fux Wa <dophuchoa1997vn@gmail.com>
2021-08-23 14:50:24 +0700

    add API screen KAMPT

--- src/api/emp.js
+++ src/api/emp.js
@@ -179,3 +179,38 @@ export const getDeliverySubAmount = async (month) => {
         });
     return data;
 };
+
+export const getKamPTRevenue = async (month) => {
+    let data = baseData
+    await axios({
+        method: "POST",
+        url: `${baseUrl}mobile/follow-impl-plan/enterprise/getKamPTRevenue?month=${month}`,
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: getToken(),
+        },
+    })
+        .then(async (res) => {
+            if (res.status == 200) {
+                if (Object.values(res.data).length > 0) {
+                    data = {
+                        data: res.data,
+                        isLoading: false,
+                        status: "success",
+                        error: null
+                    };
+                }
+            }
+        })
+        .catch(async (error) => {
+            console.log(error)
+            data = {
+                message: error.response.data.message,
+                isLoading: false,
+                status: "failed",
+                error: error.response.data
+            };
+        });
+    return data;
+};
--- src/screens/EmpHome/PlanFollow/DeliveringEnterprise/KAMPT/index.js
+++ src/screens/EmpHome/PlanFollow/DeliveringEnterprise/KAMPT/index.js
@@ -1,38 +1,97 @@
 import moment from 'moment';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { SafeAreaView, ScrollView, StatusBar, Text, View, Image } from 'react-native';
-import { Body, DatePicker, GETable, Header, ListItem, MenuItem, MenuItemShow, TextAmount } from '../../../../../comps';
+import { Body, DatePicker, GETable, Header, ListItem, Loading, MenuItem, MenuItemShow, TextAmount } from '../../../../../comps';
 import { useNavigation } from '@react-navigation/core';
 import { fontScale } from '../../../../../utils/Fonts';
 import { width } from '../../../../../utils/Dimenssion';
 import { colors } from '../../../../../utils/Colors';
 import { styles } from './style';
 import { images } from '../../../../../utils/Images';
+import { changeTime, getMonth } from '../../../../../utils/Logistics';
+import { getKamPTRevenue } from '../../../../../api/emp';
+import { showToast } from '../../../../../utils/toast';
+import { _storeData } from '../../../../../utils/Storage';
+import Toast from 'react-native-toast-message';
 
 const KAMPT = (props) => {
     const navigation = useNavigation();
     const [month, setMonth] = useState(moment(new Date()).subtract(1, 'months').format("MM/YYYY"));
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1);
+    const [lastMonth, setLastMonth] = useState(new Date().getMonth());
+    const getDatePikerValue = async () => {
+        let monthStore = await getMonth()
+        if (monthStore != undefined) {
+            setMonth(monthStore)
+            getData(monthStore)
+            var d = new Date(changeTime(monthStore))
+            setCurrentMonth(d.getMonth() + 1)
+            d.setMonth(d.getMonth() - 1);
+            setLastMonth(d.getMonth() + 1)
+        } else {
+            getData(month)
+            var d = new Date(changeTime(month))
+            setCurrentMonth(d.getMonth() + 1)
+            d.setMonth(d.getMonth() - 1);
+            setLastMonth(d.getMonth() + 1)
+        }
+    }
+
+    const getData = async (month) => {
+        setLoading(true)
+        let res = await getKamPTRevenue(month)
+        if (res.status == "success") {
+            // showToast("success", "Thành công", "Lấy dữ liệu thành công")
+            setData(res.data.data)
+            // console.log(res.data.data)
+            setLoading(false)
+        } else {
+            showToast("error", "Lỗi hệ thống", res.message)
+            setLoading(false)
+        }
+    }
+
+    const onChangeDatePicker = (date) => {
+        var d = new Date(changeTime(date))
+        setCurrentMonth(d.getMonth() + 1)
+        d.setMonth(d.getMonth() - 1);
+        setLastMonth(d.getMonth() + 1)
+
+        setData([])
+        setMonth(date)
+        getData(date)
+        _storeData("month", date)
+    }
+    useEffect(() => {
+        navigation.addListener('focus', async () => {
+            await getDatePikerValue()
+        })
+    })
+
     return (
         <SafeAreaView style={styles.container}>
+            <Toast style={{ position: "absolute", zIndex: 100 }} ref={(ref) => Toast.setRef(ref)} />
             <StatusBar translucent backgroundColor={colors.primary} />
             <Header title="DT TB do KAM PT thuộc tập DN giao" />
             <View style={{ alignSelf: "center" }}>
-                <DatePicker month={month} width={width - fontScale(120)} onChangeDate={(date) => setMonth(date)} />
+                <DatePicker month={month} width={width - fontScale(120)} onChangeDate={(date) => onChangeDatePicker(date)} />
             </View>
             <Body style={{ marginTop: fontScale(44) }} showInfo={false} />
             <View style={styles.body}>
-                <MenuItemShow value="38,000,000" style={{ marginTop: fontScale(25) }} title="Doanh thu tháng 7" titleMenuStyle={{ paddingTop: fontScale(17) }} icon={images.ic_KAMPT1} width={width - fontScale(60)} />
+                <MenuItemShow value={data.lastMonthTotal} style={{ marginTop: fontScale(25) }} title={"Doanh thu tháng " + lastMonth} titleMenuStyle={{ paddingTop: fontScale(17) }} icon={images.ic_KAMPT1} width={width - fontScale(60)} />
                 {/* --------------------- */}
                 <View style={styles.bg}>
                     <View style={{ flexDirection: "row" }}>
-                        <Text style={{ flex: 1 / 2, fontSize: fontScale(18), fontWeight: "bold", marginBottom: fontScale(10), marginTop: fontScale(40), marginLeft: fontScale(10) }}>Doanh thu tháng 8</Text>
+                        <Text style={{ flex: 1 / 2, fontSize: fontScale(18), fontWeight: "bold", marginBottom: fontScale(10), marginTop: fontScale(40), marginLeft: fontScale(10) }}>{"Doanh thu tháng " + currentMonth}</Text>
                         <Image source={images.ic_KAMPT2} style={{ width: fontScale(50), height: fontScale(50), position: "absolute", right: fontScale(30), top: -fontScale(23) }} resizeMode="contain" />
                         <Text style={{
                             flex: 1 / 2, fontSize: fontScale(18), fontWeight: "bold", marginBottom: fontScale(10), marginTop: fontScale(40), textAlign: "right",
                             marginRight: fontScale(30), color: "#35CBD6"
                         }}
-                        >18,000,000</Text>
+                        >{data.curMonthTotal}</Text>
                     </View>
 
                     <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -44,7 +103,7 @@ const KAMPT = (props) => {
                             flex: 1 / 3, fontSize: fontScale(18), fontWeight: "bold", marginBottom: fontScale(10), marginTop: fontScale(10), textAlign: "right",
                             marginRight: fontScale(30), color: "#35CBD6"
                         }}
-                        >9,000,000</Text>
+                        >{data.curMonthQual}</Text>
                     </View>
 
                     <View style={{ flexDirection: "row", alignItems: "center" }}>
@@ -56,11 +115,11 @@ const KAMPT = (props) => {
                             flex: 1 / 3, fontSize: fontScale(18), fontWeight: "bold", marginBottom: fontScale(10), marginTop: fontScale(10), textAlign: "right",
                             marginRight: fontScale(30), color: "#35CBD6"
                         }}
-                        >9,000,000</Text>
+                        >{data.curMonthNonQual}</Text>
                     </View>
                 </View>
-                {/* --------------------- */}
             </View>
+            <Loading loading={loading} />
         </SafeAreaView>
     );
 }
