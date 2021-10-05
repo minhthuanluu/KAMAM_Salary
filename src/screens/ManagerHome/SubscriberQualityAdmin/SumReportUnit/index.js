bd4eca80f64e1e5fa36b09f0f9dd75abc20fed33
John <minhthuan.luu@gmail.com>
2021-10-05 08:08:44 +0700

    optimize code

--- src/screens/ManagerHome/SubscriberQualityAdmin/SumReportUnit/index.js
+++ src/screens/ManagerHome/SubscriberQualityAdmin/SumReportUnit/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Header, DateView, Body } from '../../../../comps';
-import { styles } from './stytes';
+import { reportstyles, styles } from './stytes';
 import { colors } from '../../../../utils/Colors';
 import { text } from '../../../../utils/Text';
 import moment from 'moment';
@@ -13,11 +13,12 @@ import { getReportByUnit } from '../../../../api/manager';
 import Toast from 'react-native-toast-message';
 import { SumReportU } from "../../../../models/Admin"
 import { ToastNotif } from '../../../../utils/Logistics';
+import { ReportByUnitItem, ReportByUnitItemFinal } from './ListItem';
 
 //Branch
 const SumReportUnit = (props) => {
-    const [beginMonth, setBeginMonth] = useState(moment(new Date()).subtract(1, "months").format("MM/YYYY"))
-    const [endMonth, setEndMonth] = useState(moment(new Date()).subtract(12, "months").format("MM/YYYY"))
+    const [beginMonth, setBeginMonth] = useState(moment(new Date()).subtract(12, "months").format("MM/YYYY"))
+    const [endMonth, setEndMonth] = useState(moment(new Date()).subtract(1, "months").format("MM/YYYY"))
     const navigation = useNavigation();
     const [loading, setLoading] = useState(false);
     const isFocus = useIsFocused();
@@ -87,91 +88,5 @@ const SumReportUnit = (props) => {
         </SafeAreaView>
     );
 }
-
-const ReportByUnitItem = (props) => {
-    const { item, index } = props;
-    return <View style={[reportByUnitItem.container, { marginTop: index > 0 ? fontScale(60) : fontScale(30) }]}>
-        <Image style={reportByUnitItem.icon} source={item.icon == "BRANCH" ? images.branch : item.icon == "COMPANY" ? images.company : item.icon == "UNIT" ? images.store : null} />
-        <TouchableOpacity style={reportByUnitItem.subContainer} onPress={props.onPress}>
-            <Text style={reportByUnitItem.shopCode}>{item.shopCode}</Text>
-            <View style={{ flexDirection: "row", marginTop: fontScale(20) }}>
-                <ReportByUnitSubItem flex={1.3} title='SL TBTS' value={item.postpaid} />
-                <ReportByUnitSubItem flex={1.3} title='SL cắt huỷ' value={item.revoke} />
-                <ReportByUnitSubItem flex={2.9} title='TB chuyển Fone card' value={item.foneCard} />
-                <ReportByUnitSubItem flex={1} title='Chặn 2c' value={item.deny2C} />
-            </View>
-        </TouchableOpacity>
-    </View>
-}
-const ReportByUnitItemFinal = (props) => {
-    const { item, index } = props;
-    return <View style={[reportByUnitItem.container, props.style]}>
-        <Image style={reportByUnitItem.icon} source={images.company} />
-        <View style={{ ...reportByUnitItem.subContainer, backgroundColor: "#EFFEFF" }}>
-            <Text style={{ ...reportByUnitItem.shopCode, color: "#D19E01" }}>{item.shopName}</Text>
-            <View style={{ flexDirection: "row", marginTop: fontScale(20) }}>
-                <ReportByUnitSubItem flex={1.3} title='SL TBTS' value={item.postpaid} />
-                <ReportByUnitSubItem flex={1.3} title='SL cắt huỷ' value={item.revoke} />
-                <ReportByUnitSubItem flex={2.9} title='TB chuyển Fone card' value={item.foneCard} />
-                <ReportByUnitSubItem flex={1} title='Chặn 2c' value={item.deny2C} />
-            </View>
-        </View>
-    </View>
-}
-
-
-const ReportByUnitSubItem = ({ title, value, flex }) => {
-    return <View style={{ flex: flex, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{ color: "#9E9898", fontWeight: "bold", fontSize: fontScale(14) }}>{title}</Text>
-        <Text style={{ color: "#00BECC", fontWeight: "bold", fontSize: fontScale(14), marginTop: fontScale(11) }}>{value}</Text>
-    </View>
-}
-
-const reportByUnitItem = StyleSheet.create({
-    container: {
-
-    },
-    icon: {
-        resizeMode: "cover",
-        width: fontScale(50),
-        height: fontScale(50),
-        position: "absolute",
-        right: fontScale(20),
-        top: -fontScale(25),
-        zIndex: 10
-    },
-    shopCode: {
-        fontSize: fontScale(17),
-        fontWeight: "bold",
-        marginLeft: fontScale(10)
-    },
-    subContainer: {
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 10,
-        },
-        backgroundColor: "#fff",
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        padding: fontScale(10),
-        alignSelf: "center",
-        borderRadius: fontScale(20),
-        width: width - fontScale(20)
-    }
-});
-
-const reportstyles = StyleSheet.create({
-    container: {
-        backgroundColor: colors.primary,
-        flex: 1
-    },
-    dateView: {
-        flex: 1,
-        flexDirection: "row"
-    },
-    bodyScr: { marginTop: fontScale(10) }
-});
 
 export default SumReportUnit;
